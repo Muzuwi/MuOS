bd69fba35388ff629d17511dbd5b732db9719189
Muzuwi <kedziora99@gmail.com>
2019-11-03 22:22:43 +0100

    LibC: Separate printf into a wrapper and a main function
    This will be helpful when implementing other string format functions

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -31,26 +31,33 @@ const char hex_lookup_table_upper[16] = {
 };
 
 
-/*
-	This function takes a format string as argument and prints
-	to stdout accordingly
-	Returns: amount of characters written
-*/
-int printf(const char* format, ...){
+struct dest_t {
+	char* dest_buffer;
+	unsigned int dest_buffer_len;
+};
+
+
+int _printf_internal(const char* format, struct dest_t destination, va_list arg) {
 	char buffer[PRINTF_BUFFER_SIZE];
 
-	va_list args;
-	va_start(args, format);
+	//  For convenience
+	char* dst_bfr = destination.dest_buffer;
+
+	//  Sanity check
+	if(destination.dest_buffer_len == 0 || destination.dest_buffer == 0) return 0;
 
 	//  Total amount of characters written to stdout
 	unsigned int total_chars_written = 0;
-
+	//  Position in the format buffer
 	unsigned int format_cnt = 0;
+	//  Position in the destination buffer
+	unsigned int dst_cnt = 0;
+
 	while(format[format_cnt] != '\0'){
 		//  Character escape sequence \x
 		if(format[format_cnt] == '\\'){
 			if(format[format_cnt+1] != '\0'){
-				putchar(format[format_cnt+1]);
+				dst_bfr[dst_cnt++] = format[format_cnt+1];
 				format_cnt += 2;
 				total_chars_written++;
 				continue;
@@ -60,7 +67,7 @@ int printf(const char* format, ...){
 		if(format[format_cnt] == '%'){
 			//  Escape seq %%
 			if(format[format_cnt+1] != '\0' && format[format_cnt+1] == '%'){
-				putchar(format[format_cnt]);
+				dst_bfr[dst_cnt++] = format[format_cnt];
 				format_cnt += 2;
 				total_chars_written++;
 				continue;
@@ -73,13 +80,13 @@ int printf(const char* format, ...){
 
 			switch(format[format_cnt+1]){
 				case 'c': {
-					char ch = (char)va_arg(args, int);
-					putchar(ch);
+					char ch = (char)va_arg(arg, int);
+					dst_bfr[dst_cnt++] = ch;
 					total_chars_written++;
 					break;
 				}
 				case 'i':{
-					int num = va_arg(args, int);
+					int num = va_arg(arg, int);
 
 					int workPointer = 0;
 					while(num != 0){
@@ -90,7 +97,7 @@ int printf(const char* format, ...){
 					//  Correct pointer to point to last character in buffer
 					workPointer--;
 					while(workPointer >= 0){
-						putchar(buffer[workPointer--]);
+						dst_bfr[dst_cnt++] = buffer[workPointer--];
 						total_chars_written++;
 					}
 
@@ -98,7 +105,7 @@ int printf(const char* format, ...){
 				}
 				case 'x':
 				case 'X':{
-					int num = va_arg(args, int);
+					int num = va_arg(arg, int);
 					unsigned int mask = 15;
 					int block = sizeof(int)*2 - 1;
 					while(block >= 0){
@@ -108,17 +115,18 @@ int printf(const char* format, ...){
 						} else {
 							ch = hex_lookup_table_upper[(num >> block*4) & mask];
 						}
-						putchar(ch);
+						dst_bfr[dst_cnt++] = ch;
 						total_chars_written++;
 						block--;
 					}
 					break;
 				}
 				case 's':{
-					char* str = va_arg(args, char*);
+					char* str = va_arg(arg, char*);
+					if(str == 0) break;
 					unsigned int ptr = 0;
 					while(str[ptr] != '\0'){
-						putchar(str[ptr]);	
+						dst_bfr[dst_cnt++] = str[ptr];
 						total_chars_written++;
 						ptr++;
 					} 
@@ -129,13 +137,37 @@ int printf(const char* format, ...){
 			}
 			format_cnt++;
 		} else {
-			putchar(format[format_cnt]);
+			dst_bfr[dst_cnt++] = format[format_cnt];
 			total_chars_written++;
 		}
 		format_cnt++;
 	}
 
-	va_end(args);
+	//  Null terminate
+	dst_bfr[dst_cnt++] = '\0';
 
 	return total_chars_written;
 }
+
+
+/*
+	This function takes a format string as argument and prints
+	to stdout accordingly
+	Returns: amount of characters written
+*/
+int printf(const char* format, ...){
+	char buf[PRINTF_BUFFER_SIZE];
+
+	struct dest_t destination;
+	destination.dest_buffer = buf;
+	destination.dest_buffer_len = PRINTF_BUFFER_SIZE;
+
+	va_list args;
+	
+	va_start(args, format);
+	_printf_internal(format, destination, args);
+	va_end(args);
+
+	puts(buf);
+}
+

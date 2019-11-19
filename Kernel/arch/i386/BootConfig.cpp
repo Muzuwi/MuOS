19c9e978a519e0f277394da6fd9d685a30a69133
Muzuwi <kedziora99@gmail.com>
2019-11-19 15:39:16 +0100

    uKernel/BootConfig: Address sanitization
    Added simple sanitization for addresses passed in the multiboot structure,
    now that paging is fully set up after jumping to c++ kernel.

--- Kernel/arch/i386/BootConfig.cpp
+++ Kernel/arch/i386/BootConfig.cpp
@@ -1,19 +1,41 @@
 #include <arch/i386/BootConfig.hpp>
 #include <arch/i386/MemManager.hpp>
 #include <arch/i386/multiboot.hpp>
+#include <arch/i386/paging.hpp>
 #include <kernel/kdebugf.hpp>
 
 extern uint32_t _ukernel_virtual_offset;
 
+/*
+	This function takes a pointer from a multiboot structure
+	and makes sure that memory referenced by it is mapped, and
+	returns a pointer to it in the kernel address space
+*/
+uint32_t* sanitize_multiboot_pointer(uint32_t* pointer) {
+	if(!pointer) {
+		return nullptr;
+	}
+
+	uint32_t* virtual_address = (uint32_t*)((uint32_t)pointer + (uint32_t)&_ukernel_virtual_offset); 
+
+	if(!Paging::is_present(virtual_address)) {
+		Paging::allocate_page((void*)pointer, (void*)virtual_address);
+	}
+
+	kdebugf("[multiboot_sanitizer] Sanitized %x to %x\n", (uint32_t)pointer, (uint32_t)virtual_address);
+
+	return virtual_address;
+}
+
+
 void BootConfig::parse_multiboot_structure(uintptr_t* multiboot_info){
-	//  At this point, the first 1MiB is not 1:1 mapped anymore, we need to 
-	//  add the virtual offset to get the virtual address to it
-	uint32_t* phys_multiboot_info = (uint32_t*)((uintptr_t)multiboot_info + (uintptr_t)&_ukernel_virtual_offset);
+	uint32_t* phys_multiboot_info = sanitize_multiboot_pointer(multiboot_info);
 
 	if(!phys_multiboot_info){
 		kerrorf("[BootConfig] No valid multiboot structure passed, panic!\n");
 		asm volatile("cli\nhlt");
 	}
+	
 	kdebugf("[BootConfig] Multiboot structure at %x\n", phys_multiboot_info);
 
 	auto flags = (uint32_t)phys_multiboot_info[0];
@@ -26,8 +48,11 @@ void BootConfig::parse_multiboot_structure(uintptr_t* multiboot_info){
 		kdebugf("boot device: %x\n", phys_multiboot_info[3]);
 
 	if(flags & multiboot_flag_t::MULTIBOOT_CMDLINE){
-		uint32_t* address = (uint32_t*)(phys_multiboot_info[4] + (uint32_t)&_ukernel_virtual_offset);
-		kdebugf("kernel arguments: %s\n", address);
+		uint32_t* address = sanitize_multiboot_pointer((uint32_t*)phys_multiboot_info[4]);
+
+		if(address) {
+			kdebugf("kernel arguments: %s\n", address);
+		}
 	}
 
 	if(flags & multiboot_flag_t::MULTIBOOT_MODULES)
@@ -40,7 +65,9 @@ void BootConfig::parse_multiboot_structure(uintptr_t* multiboot_info){
 		MemManager::get().parse_multiboot_mmap(&phys_multiboot_info[11]);
 
 	if(flags & multiboot_flag_t::MULTIBOOT_BOOTNAME){
-		uint32_t* address = (uint32_t*)(phys_multiboot_info[16] + (uint32_t)&_ukernel_virtual_offset);
-		kdebugf("bootloader name: %s\n", address);
+		uint32_t* address = sanitize_multiboot_pointer((uint32_t*)phys_multiboot_info[16]);
+		if(address) {
+			kdebugf("bootloader name: %s\n", address);
+		}
 	}
 }

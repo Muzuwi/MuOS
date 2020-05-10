caea3f7c31802b05f50de07c9a0e60c7c3ef7ef2
Muzuwi <kedziora99@gmail.com>
2020-05-10 14:44:59 +0200

    LibGeneric: Iterator classes

---
+++ Library/LibGeneric/BidirectionalIterator.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <LibGeneric/Iterator.hpp>
+
+namespace gen {
+	/*
+    *  Class representing a bidirectional iterator, that can be incremented and decremented.
+    */
+	template<class T>
+	class BidirectionalIterator : public Iterator<T> {
+	public:
+		BidirectionalIterator(const T&);
+
+		BidirectionalIterator& operator++();
+		BidirectionalIterator  operator++(int);
+		BidirectionalIterator& operator--();
+		BidirectionalIterator  operator--(int);
+
+		bool operator==(const BidirectionalIterator& rhs) const;
+		bool operator!=(const BidirectionalIterator& rhs) const;
+
+		typename Iterator<T>::value_type operator*();
+	};
+}
---
+++ Library/LibGeneric/Iterator.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace gen {
+	/*
+    *  Iterator base class
+    */
+	template<class T, class Pointer = T*, class Reference = T&>
+	class Iterator {
+	public:
+		typedef T           value_type;
+		typedef Pointer     ptr_type;
+		typedef Reference   ref_type;
+	};
+}

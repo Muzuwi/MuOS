1d1941e4ed24f3903321efa57c3afc3c6b3f406f
Muzuwi <kedziora99@gmail.com>
2020-05-25 21:00:25 +0200

    Debug: Nicer kpanic messages (function name instead of file)

--- Kernel/Debug/kpanic.cpp
+++ Kernel/Debug/kpanic.cpp
@@ -2,7 +2,7 @@
 #include <Kernel/Debug/kdebugf.hpp>
 
 [[noreturn]] void _kpanic_internal(const char* file, int line){
-	kerrorf("!!! Kernel panic at %s, line %i\n", file, (int)line);
+	kerrorf("!!! Kernel panic at '%s', line %i\n", file, (int)line);
 	kerrorf("!!! System halted\n");
 	asm volatile("cli\nhlt");
 	while(1);
--- Kernel/include/Kernel/Debug/kpanic.hpp
+++ Kernel/include/Kernel/Debug/kpanic.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
 [[noreturn]] void _kpanic_internal(const char*, int);
-#define kpanic() _kpanic_internal(__FILE__, __LINE__)
+#define kpanic() _kpanic_internal(__PRETTY_FUNCTION__, __LINE__)

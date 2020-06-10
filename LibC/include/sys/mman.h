24cfe8fd1971845bfcc4315f7d260fdf11c14766
Muzuwi <kedziora99@gmail.com>
2020-06-10 20:20:10 +0200

    LibC: sys/types, errno, mman

---
+++ LibC/include/sys/mman.h
@@ -0,0 +1,12 @@
+#ifndef __MMAN_H
+#define __MMAN_H
+
+#define PROT_READ   0x1
+#define PROT_WRITE  0x2
+#define PROT_EXEC   0x4
+
+#define MAP_SHARED  0x1
+#define MAP_PRIVATE 0x2
+#define MAP_FIXED   0x4
+
+#endif
---
+++ LibC/include/sys/types.h
@@ -0,0 +1,10 @@
+#ifndef LIBC_SYSTYPES_H
+#define LIBC_SYSTYPES_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+typedef int32_t ssize_t;
+typedef int32_t off_t;
+
+#endif

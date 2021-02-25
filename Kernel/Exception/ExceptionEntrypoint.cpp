745a442972176e22d0831fb784a14a52138cb8b5
Muzuwi <kedziora99@gmail.com>
2021-02-25 23:33:44 +0100

    Exception: Additional debug print on exceptions in processes

--- Kernel/Exception/ExceptionEntrypoint.cpp
+++ Kernel/Exception/ExceptionEntrypoint.cpp
@@ -1,6 +1,7 @@
 #include <Kernel/Exception/Exception.hpp>
 #include <Kernel/Debug/kdebugf.hpp>
 #include <Kernel/Debug/kpanic.hpp>
+#include <Kernel/Process/Process.hpp>
 
 static Exception::HandlerFunction s_exception_handlers[32] {
 		nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
@@ -18,7 +19,9 @@ extern "C" void _kernel_exception_entrypoint(size_t vector, PtraceRegs* interrup
 	kerrorf("rsi=%x%x rdi=%x%x rbp=%x%x rip=%x%x\n", S(pt->rsi), S(pt->rdi),S(pt->rbp),S(pt->rip));
 	kerrorf(" r8=%x%x  r9=%x%x r10=%x%x r11=%x%x\n", S(pt->r8), S(pt->r9),S(pt->r10),S(pt->r11));
 	kerrorf("r12=%x%x r13=%x%x r14=%x%x r15=%x%x\n", S(pt->r12), S(pt->r13),S(pt->r14),S(pt->r15));
-	kerrorf("rflags=%x%x cs=%i origin=%x%x\n", S(pt->rflags), pt->cs, S(pt->origin));
+	kerrorf("rsp=%x%x rflags=%x%x cs=%i origin=%x%x\n", S(pt->rsp),S(pt->rflags), pt->cs, S(pt->origin));
+	if(Process::current())
+		kerrorf("process=%x%x, pid=%i, task_frame=%x%x, stack_frame=%x%x\n", S(Process::current()), Process::current()->pid(), S(Process::current()->frame()), S(interrupt_stack_frame));
 
 	if(!s_exception_handlers[vector]) {
 		kerrorf("[Exception] Unhandled exception vector=%x\n", vector);

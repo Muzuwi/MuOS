e134ed3eebbcda9e14fb2b867d5717178123ca39
Muzuwi <kedziora99@gmail.com>
2021-02-25 23:22:14 +0100

    CPU: RDRAND support CPUID getters

--- Kernel/Arch/i386/CPUID.cpp
+++ Kernel/Arch/i386/CPUID.cpp
@@ -19,3 +19,9 @@ bool CPUID::has_SEP() {
 	__get_cpuid(0x1, &eax, &_unused, &_unused, &edx);
 	return edx & (1u << 11u);
 }
+
+bool CPUID::has_RDRAND() {
+	unsigned int eax, ecx, _unused, edx;
+	__get_cpuid(0x1, &eax, &_unused, &ecx, &edx);
+	return ecx & (1u << 30u);
+}
--- Kernel/include/Arch/i386/CPUID.hpp
+++ Kernel/include/Arch/i386/CPUID.hpp
@@ -4,4 +4,5 @@ namespace CPUID {
 	bool has_huge_pages();
 	bool has_NXE();
 	bool has_SEP();
+	bool has_RDRAND();
 }

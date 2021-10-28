ebc29b59c72b6ecf9bd9cf11d8684623f950292a
Muzuwi <kedziora99@gmail.com>
2021-10-28 22:37:00 +0200

    Misc: Remove old cruft

--- Kernel/Debug/kdebugf.cpp
+++ Kernel/Debug/kdebugf.cpp
@@ -18,23 +18,6 @@ int kdebugf(const char* format, ...){
 
 	va_end(args);
 
-//	Ports::out(0x3f8, '\u001b');
-//	Ports::out(0x3f8, '[');
-//	Ports::out(0x3f8, '3');
-//	Ports::out(0x3f8, '2');
-//	Ports::out(0x3f8, 'm');
-
-	//  TODO: Maybe don't use strlen here?
-//	for(size_t i = 0; i < strlen(buffer); i++){
-//		Ports::out(0x3f8, buffer[i]);
-//	}
-
-
-//	Ports::out(0x3f8, '\u001b');
-//	Ports::out(0x3f8, '[');
-//	Ports::out(0x3f8, '0');
-//	Ports::out(0x3f8, 'm');
-
 	Serial::write_str(Serial::Port::COM0, "\u001b[32m");
 	Serial::write_str(Serial::Port::COM0, buffer);
 	Serial::write_str(Serial::Port::COM0, "\u001b[0m");
@@ -50,35 +33,15 @@ int kerrorf(const char* format, ...){
 	va_list args;
 	va_start(args, format);
 
-	//  TODO: Maybe don't use strlen here?
 	int c = vsnprintf(buffer, KDEBUGF_BUFFER_SIZE, format, args);
 
 	va_end(args);
-
-//	Ports::out(0x3f8, '\u001b');
-//	Ports::out(0x3f8, '[');
-//	Ports::out(0x3f8, '1');
-//	Ports::out(0x3f8, ';');
-//	Ports::out(0x3f8, '9');
-//	Ports::out(0x3f8, '1');
-//	Ports::out(0x3f8, 'm');
-//
-//
-//	for(size_t i = 0; i < strlen(buffer); i++){
-//		Ports::out(0x3f8, buffer[i]);
-//	}
-//
-//	Ports::out(0x3f8, '\u001b');
-//	Ports::out(0x3f8, '[');
-//	Ports::out(0x3f8, '0');
-//	Ports::out(0x3f8, 'm');
 
 	Serial::write_str(Serial::Port::COM0, "\u001b[1;91m");
 	Serial::write_str(Serial::Port::COM0, buffer);
 	Serial::write_str(Serial::Port::COM0, "\u001b[0m");
 
 	TTY::prints(buffer);
-
 
 	return c;
 }

64ab61cd72dfd4c57e3742b43ca0f171365ceffc
Muzuwi <kedziora99@gmail.com>
2022-02-13 20:22:01 +0100

    PMM: Proper locking

--- Kernel/Memory/PMM.cpp
+++ Kernel/Memory/PMM.cpp
@@ -4,6 +4,8 @@
 #include <Memory/PMM.hpp>
 #include <Multiboot/MultibootInfo.hpp>
 #include <Symbols.hpp>
+#include <SMP/SMP.hpp>
+#include <Process/Thread.hpp>
 
 PMM PMM::s_instance {};
 
@@ -25,7 +27,7 @@ void PMM::init_regions(PhysPtr<MultibootInfo> multiboot_info) {
 		klogf_static("[PMM] {x} - {x}: ", start, end);
 
 		if((uintptr_t)m_physical_end.get() < end) {
-			m_physical_end = PhysAddr{(void*)end};
+			m_physical_end = PhysAddr { (void*)end };
 		}
 
 		switch(pointer->type()) {
@@ -43,7 +45,7 @@ void PMM::init_regions(PhysPtr<MultibootInfo> multiboot_info) {
 						start = 0x1000;
 						range -= 0x1000;
 					}
-					m_mem16_regions.push_back({PhysAddr {(void*)start}, range});
+					m_mem16_regions.push_back({ PhysAddr { (void*)start }, range });
 					break;
 				}
 
@@ -54,12 +56,12 @@ void PMM::init_regions(PhysPtr<MultibootInfo> multiboot_info) {
 				if(start < kernel_phys_end) {
 					if(end > kernel_phys_end) {
 						if(start < kernel_phys_start) {
-							m_normal_regions.push_back({PhysAddr {(void*)start}, kernel_phys_start - start});
+							m_normal_regions.push_back({ PhysAddr { (void*)start }, kernel_phys_start - start });
 						}
-						m_normal_regions.push_back({PhysAddr {(void*)kernel_phys_end}, end - kernel_phys_end});
+						m_normal_regions.push_back({ PhysAddr { (void*)kernel_phys_end }, end - kernel_phys_end });
 					}
 				} else {
-					m_normal_regions.push_back({PhysAddr {(void*)start}, range});
+					m_normal_regions.push_back({ PhysAddr { (void*)start }, range });
 				}
 
 				break;
@@ -115,59 +117,83 @@ void PMM::init_deferred_allocators() {
 	}
 }
 
-
 /*
  *  Normal pool regions
  */
 [[nodiscard]] KOptional<PAllocation> PMM::allocate(size_t count_order) {
-	for(auto& region : m_normal_regions) {
-		auto ret = region.allocator().allocate(count_order);
-		if(ret.has_value()) {
-			auto page = ret.unwrap();
-			auto allocation = PAllocation(page, count_order);
-			return KOptional<PAllocation> {allocation};
-		}
-	}
+	auto* thread = SMP::ctb().current_thread();
 
-	klogf_static("[PMM] Allocation failure for order of page_count={}\n", (1u << count_order));
-	return KOptional<PAllocation> {};
+	if(thread) { thread->preempt_disable(); }
+	m_pmm_lock.lock();
+	auto result = [this, count_order]() -> auto {
+		for(auto& region : m_normal_regions) {
+			auto ret = region.allocator().allocate(count_order);
+			if(ret.has_value()) {
+				auto page = ret.unwrap();
+				auto allocation = PAllocation(page, count_order);
+				return KOptional<PAllocation> { allocation };
+			}
+		}
+		return KOptional<PAllocation> {};
+	}();
+	m_pmm_lock.unlock();
+	if(thread) { thread->preempt_enable(); }
+
+	return result;
 }
 
 void PMM::free(PAllocation const& allocation) {
+	auto* thread = SMP::ctb().current_thread();
+
+	if(thread) { thread->preempt_disable(); }
+	m_pmm_lock.lock();
 	for(auto& region : m_normal_regions) {
 		if(region.contains(allocation.base()) && region.contains(allocation.end())) {
 			region.allocator().free(allocation.base(), allocation.order());
-			return;
+			break;
 		}
 	}
-
-	klogf_static("[PMM] Deallocation failure\n");
+	m_pmm_lock.unlock();
+	if(thread) { thread->preempt_enable(); }
 }
 
 /*
  *  Lowmem pool regions
  */
-KOptional<PhysAddr> PMM::allocate_lowmem() {
-	for(auto& region : m_mem16_regions) {
-		auto ret = region.allocator().allocate(0);
-		if(ret.has_value()) {
-			auto page = ret.unwrap();
-			return KOptional<PhysAddr> {page};
-		}
-	}
+[[nodiscard]] KOptional<PhysAddr> PMM::allocate_lowmem() {
+	auto* thread = SMP::ctb().current_thread();
 
-	return KOptional<PhysAddr> {};
+	if(thread) { thread->preempt_disable(); }
+	m_pmm_lock.lock();
+	auto result = [this]() -> auto {
+		for(auto& region : m_mem16_regions) {
+			auto ret = region.allocator().allocate(0);
+			if(ret.has_value()) {
+				auto page = ret.unwrap();
+				return KOptional<PhysAddr> { page };
+			}
+		}
+		return KOptional<PhysAddr> {};
+	}();
+	m_pmm_lock.unlock();
+	if(thread) { thread->preempt_enable(); }
+
+	return result;
 }
 
 void PMM::free_lowmem(PhysAddr addr) {
+	auto* thread = SMP::ctb().current_thread();
+
+	if(thread) { thread->preempt_disable(); }
+	m_pmm_lock.lock();
 	for(auto& region : m_mem16_regions) {
 		if(region.contains(addr)) {
 			region.allocator().free(addr, 0);
-			return;
+			break;
 		}
 	}
-
-	klogf_static("[PMM] Failed freeing lowmem at {}\n", Format::ptr(addr.get()));
+	m_pmm_lock.unlock();
+	if(thread) { thread->preempt_enable(); }
 }
 
 PhysAddr PMM::physical_end_addr() {
--- Kernel/Memory/PMM.hpp
+++ Kernel/Memory/PMM.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <stdint.h>
 #include <stddef.h>
 #include <Structs/KOptional.hpp>
@@ -8,30 +9,35 @@
 #include <Memory/Allocators/PRegion.hpp>
 #include <LibGeneric/Vector.hpp>
 #include <Memory/kmalloc.hpp>
-#include "Memory/Allocators/PageBitmapAllocator.hpp"
-
+#include <Memory/Allocators/PageBitmapAllocator.hpp>
+#include <LibGeneric/Spinlock.hpp>
 
 class MultibootInfo;
 
 class PMM {
 	static PMM s_instance;
-	PMM() noexcept {}
 
-	PhysAddr m_physical_end;
-	gen::Vector<PRegion, KMalloc::BootstrapAllocator> m_mem16_regions;  //  "Lowmem" - accessible from 16-bit
-	gen::Vector<PRegion, KMalloc::BootstrapAllocator> m_normal_regions; //  Accessible from 32-bit
+	PMM() noexcept = default;
+
+	gen::Spinlock m_pmm_lock {};
+	PhysAddr m_physical_end {};
+	gen::Vector<PRegion, KMalloc::BootstrapAllocator> m_mem16_regions {};  //  "Lowmem" - accessible from 16-bit
+	gen::Vector<PRegion, KMalloc::BootstrapAllocator> m_normal_regions {}; //  Accessible from 32-bit
 public:
 	static PMM& instance() {
 		return s_instance;
 	}
 
 	[[nodiscard]] KOptional<PAllocation> allocate(size_t count_order = 0);
+
 	void free(PAllocation const&);
 
 	[[nodiscard]] KOptional<PhysAddr> allocate_lowmem();
+
 	void free_lowmem(PhysAddr);
 
 	void init_regions(PhysPtr<MultibootInfo>);
+
 	void init_deferred_allocators();
 
 	PhysAddr physical_end_addr();

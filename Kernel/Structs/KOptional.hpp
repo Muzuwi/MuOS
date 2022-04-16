3ef64eb46370ec22072e5313d1e61df6830072fa
Muzuwi <kedziora99@gmail.com>
2022-04-17 00:45:43 +0200

    Kernel: Replace ad-hoc KOptional with gen::Optional

--- Kernel/Structs/KOptional.hpp
+++ Kernel/Structs/KOptional.hpp
@@ -1,28 +1,5 @@
 #pragma once
-#include <Debug/kassert.hpp>
+#include <LibGeneric/Optional.hpp>
 
-//  FIXME:  Hacky hack
-
-template<class T>
-class KOptional {
-	T m_value;
-	bool m_has_value;
-
-	void _construct_none() { m_has_value = false; }
-
-	void _construct_some(T value) {
-		m_has_value = true;
-		m_value = value;
-	}
-public:
-	KOptional() { _construct_none(); }
-
-	KOptional(T val) { _construct_some(val); }
-
-	T unwrap() {
-		assert(m_has_value);
-		return m_value;
-	}
-
-	bool has_value() { return m_has_value; }
-};
+template<typename T>
+using KOptional = gen::Optional<T>;

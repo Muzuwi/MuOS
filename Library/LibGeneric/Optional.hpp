2ee84c290e748980035e6762acdc74ea77608285
Muzuwi <kedziora99@gmail.com>
2022-04-17 00:36:17 +0200

    LibGeneric: Add gen::Optional

--- Library/LibGeneric/Optional.hpp
+++ Library/LibGeneric/Optional.hpp
@@ -3,6 +3,8 @@
 #include <LibGeneric/Move.hpp>
 #ifndef __is_kernel_build__
 #	include <cassert>
+#else
+#	include <Debug/kassert.hpp>
 #endif
 
 namespace gen {
@@ -22,12 +24,22 @@ namespace gen {
 		OptType m_type;
 		T m_data;
 	public:
+		/*
+		 *  Constructs an Optional object containing no value.
+		 */
 		constexpr Optional() noexcept
 		    : m_type(OptType::None) {}
 
+		/*
+		 *  Constructs an Optional object containing no value.
+		 *  Explicit version of the default constructor.
+		 */
 		constexpr Optional(gen::__NullOpt) noexcept
 		    : m_type(OptType::None) {}
 
+		/*
+		 *  Copies an Optional object.
+		 */
 		constexpr Optional(Optional const& other) noexcept {
 			if(other.has_value()) {
 				m_type = OptType::Some;
@@ -37,8 +49,22 @@ namespace gen {
 			}
 		}
 
-		constexpr Optional(Optional&& other) noexcept = delete;
+		/*
+		 *  Constructs an Optional object from a different one, and moves its data
+		 *  into the new one.
+		 */
+		constexpr Optional(Optional&& other) noexcept {
+			if(other.has_value()) {
+				m_type = OptType::Some;
+				gen::construct_at(&m_data, gen::move(other.m_data));
+			} else {
+				m_type = OptType::None;
+			}
+		}
 
+		/*
+		 *  Copy conversion constructor
+		 */
 		template<class U>
 		constexpr Optional(Optional<U> const& other) {
 			if(other.has_value()) {
@@ -49,13 +75,26 @@ namespace gen {
 			}
 		}
 
+		/*
+		 *  Move conversion constructor
+		 */
 		template<class U>
-		constexpr Optional(Optional<U>&& other) noexcept = delete;
+		constexpr Optional(Optional<U>&& other) noexcept {
+			if(other.has_value()) {
+				m_type = OptType::Some;
+				gen::construct_at(&m_data, gen::move(other.m_data));
+			} else {
+				m_type = OptType::None;
+			}
+		}
 
+		/*
+		 *  Constructs an Optional from a value convertible to T.
+		 */
 		template<class U = T>
-		constexpr Optional(U&& value) noexcept
+		constexpr Optional(U value) noexcept
 		    : m_type(OptType::Some)
-		    , m_data(gen::forward<U>(value)) {}
+		    , m_data(gen::move(value)) {}
 
 		constexpr Optional& operator=(gen::__NullOpt) noexcept {
 			if(has_value()) {
@@ -74,32 +113,55 @@ namespace gen {
 			return *this;
 		}
 
-		constexpr Optional& operator=(Optional&& other) noexcept = delete;
+		constexpr Optional& operator=(Optional&& other) noexcept {
+			reset();
+			if(!other.has_value()) {
+				return *this;
+			}
+			m_type = other.m_type;
+			gen::construct_at(&m_data, gen::move(other.m_data));
+			return *this;
+		}
 
 		template<class U = T>
-		constexpr Optional& operator=(U&& value) {
+		constexpr Optional& operator=(U value) {
 			if(has_value()) {
 				reset();
 			}
 			m_type = OptType::Some;
-			gen::construct_at(&m_data, gen::forward<U>(value));
+			gen::construct_at(&m_data, gen::move(value));
 			return *this;
 		}
 
+		/*
+		 *  Returns the status of the Optional - true if the Optional contains a value.
+		 */
 		constexpr bool has_value() const { return m_type == OptType::Some; }
 
 		constexpr explicit operator bool() { return has_value(); }
 
+		/*
+		 *  Returns a reference to the stored data.
+		 *  If no data is currently stored, triggers a panic.
+		 */
 		constexpr T& unwrap() {
 			assert(has_value());
 			return m_data;
 		}
 
+		/*
+		 *  Returns a const reference to the stored data.
+		 *  If no data is currently stored, triggers a panic.
+		 */
 		constexpr T const& unwrap() const {
 			assert(has_value());
 			return m_data;
 		}
 
+		/*
+		 *  Returns the currently stored data, or a specified default value if no
+		 *  value is currently stored.
+		 */
 		template<class U>
 		constexpr T unwrap_or_default(U&& default_value) const {
 			if(has_value()) {
@@ -109,6 +171,10 @@ namespace gen {
 			}
 		}
 
+		/*
+		 *  Returns the currently stored data by moving it out of the Optional.
+		 *  If no value is currently stored, a specified default value is returned instead.
+		 */
 		template<class U>
 		constexpr T unwrap_or_default(U&& default_value) {
 			if(has_value()) {
@@ -118,6 +184,9 @@ namespace gen {
 			}
 		}
 
+		/*
+		 *  Resets the Optional object and destroys the stored data if any is available.
+		 */
 		constexpr void reset() {
 			if(!has_value()) {
 				return;
@@ -127,6 +196,9 @@ namespace gen {
 			gen::destroy_at(&m_data);
 		}
 
+		/*
+		 *  Resets the Optional object, and constructs the newly stored value in-place.
+		 */
 		template<class... Args>
 		constexpr T& emplace(Args&&... args) {
 			if(has_value()) {
--- Library/LibGeneric/Platform/Native/Tests/Optional.cpp
+++ Library/LibGeneric/Platform/Native/Tests/Optional.cpp
@@ -18,10 +18,69 @@ TEST_CASE("gen::Optional", "[structs]") {
 		REQUIRE(a.has_value());
 	}
 
+	SECTION("value getters") {
+		gen::Optional<int> a { 5 };
+		SECTION("unwrap") {
+			REQUIRE(a.unwrap() == 5);
+		}
+		SECTION("unwrap_or_default") {
+			REQUIRE(a.unwrap_or_default(5345) == 5);
+			gen::Optional<int> b {};
+			REQUIRE(b.unwrap_or_default(5345) == 5345);
+		}
+	}
+
 	SECTION("direct initialized copy") {
 		gen::Optional<int> a { 5 };
 		gen::Optional<int> b { a };
 
 		REQUIRE(a.has_value());
+		REQUIRE(b.has_value());
+		REQUIRE(a.unwrap() == 5);
+		REQUIRE(b.unwrap() == 5);
+	}
+
+	SECTION("emplace") {
+		gen::Optional<int> a {};
+		REQUIRE(!a.has_value());
+		a.emplace(5);
+		REQUIRE(a.has_value());
+		REQUIRE(a.unwrap() == 5);
+	}
+
+	SECTION("operator=") {
+		gen::Optional<int> a { 1337 };
+		REQUIRE(a.has_value());
+
+		SECTION("with nullopt") {
+			a = gen::nullopt;
+			REQUIRE(!a.has_value());
+		}
+
+		SECTION("with value") {
+			a = 5;
+			REQUIRE(a.has_value());
+			REQUIRE(a.unwrap() == 5);
+		}
+
+		SECTION("with non-null lvalue") {
+			auto v = gen::Optional<int> { 46 };
+			a = v;
+			REQUIRE(a.has_value());
+			REQUIRE(a.unwrap() == 46);
+		}
+
+		SECTION("with non-null rvalue") {
+			a = gen::Optional<int> { 36 };
+			REQUIRE(a.has_value());
+			REQUIRE(a.unwrap() == 36);
+		}
+	}
+
+	SECTION("reset") {
+		gen::Optional<int> a { 1337 };
+		REQUIRE(a.has_value());
+		a.reset();
+		REQUIRE(!a.has_value());
 	}
 }

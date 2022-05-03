724b6dc7d7b6d9a9856ee37188eca8ad9eeea24c
Muzuwi <kedziora99@gmail.com>
2022-05-03 22:33:24 +0200

    LibGeneric: Modernize LockGuard and add move semantics

--- Library/LibGeneric/LockGuard.hpp
+++ Library/LibGeneric/LockGuard.hpp
@@ -5,13 +5,16 @@ namespace gen {
 	class LockGuard {
 		LockType& m_lock;
 	public:
-		LockGuard(LockType& lock) noexcept
+		constexpr explicit LockGuard(LockType& lock) noexcept
 		    : m_lock(lock) {
 			m_lock.lock();
 		}
 
-		~LockGuard() noexcept { m_lock.unlock(); }
+		constexpr LockGuard(LockGuard&& lg) noexcept
+		    : m_lock(lg) {}
 
-		LockGuard(LockGuard const&) = delete;
+		constexpr LockGuard(LockGuard const&) = delete;
+
+		constexpr ~LockGuard() noexcept { m_lock.unlock(); }
 	};
 }

d38994f075c1a1b720cee4e81172473c80927387
Muzuwi <kedziora99@gmail.com>
2023-11-29 21:17:32 +0100

    Misc: Add nodiscard attribute to core::Error

--- Kernel/Core/Error/Error.hpp
+++ Kernel/Core/Error/Error.hpp
@@ -3,7 +3,7 @@
 namespace core {
 	/**	Common kernel error enumeration.
 	 */
-	enum class Error {
+	enum class [[nodiscard("functions returning core::Error must be checked for failures")]] Error {
 		Ok = 0,
 	};
 }

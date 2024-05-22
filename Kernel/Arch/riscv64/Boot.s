802ee4ecf34da59af36a7dfedcb2ba8f4a8b99b4
Muzuwi <kedziora99@gmail.com>
2024-05-22 21:22:34 +0200

    Arch: RV64: Add simple UART hello world
    For now, RV64 support is targetting HiFive Unleashed, so
    add a sample UART hello world during boot.

--- Kernel/Arch/riscv64/Boot.s
+++ Kernel/Arch/riscv64/Boot.s
@@ -3,5 +3,25 @@
 
 .globl _entrypoint_riscv64
 _entrypoint_riscv64:
+    # hifive unleashed UART
+    li t1, 0x10010000
+
+    # set txen=1
+    li t0, 0x1
+    sw t0, 8(t1)
+
+    li t0, 'H'
+    sw t0, 0(t1)
+    li t0, 'e'
+    sw t0, 0(t1)
+    li t0, 'l'
+    sw t0, 0(t1)
+    li t0, 'l'
+    sw t0, 0(t1)
+    li t0, 'o'
+    sw t0, 0(t1)
+    li t0, '!'
+    sw t0, 0(t1)
+.loop:
     wfi
-    j _entrypoint_riscv64
+    j .loop

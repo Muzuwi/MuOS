0e60a8759818102b783fd8c05c8a364251858618
Muzuwi <kedziora99@gmail.com>
2024-07-24 22:25:28 +0200

    x86_64: Platform: Change to <>-style includes

--- Kernel/Arch/x86_64/Platform.cpp
+++ Kernel/Arch/x86_64/Platform.cpp
@@ -1,19 +1,19 @@
 #include <Arch/Interface.hpp>
+#include <Arch/x86_64/ACPI.hpp>
+#include <Arch/x86_64/APIC.hpp>
 #include <Arch/x86_64/CPU.hpp>
 #include <Arch/x86_64/Interrupt.hpp>
 #include <Arch/x86_64/MP/Boot.hpp>
 #include <Arch/x86_64/MP/ExecutionEnvironment.hpp>
 #include <Arch/x86_64/PCI/PCI.hpp>
 #include <Arch/x86_64/Serial.hpp>
+#include <Arch/x86_64/SerialConsole.hpp>
 #include <Arch/x86_64/VGAConsole.hpp>
 #include <Core/Error/Error.hpp>
 #include <Core/MP/MP.hpp>
 #include <Memory/VMM.hpp>
 #include <Syscalls/Syscall.hpp>
 #include <SystemTypes.hpp>
-#include "ACPI.hpp"
-#include "APIC.hpp"
-#include "Arch/x86_64/SerialConsole.hpp"
 
 CREATE_LOGGER("boot::x86_64", core::log::LogLevel::Debug);
 

255af2e3d0c3a924061b489a04f24482afb7b261
Muzuwi <kedziora99@gmail.com>
2024-08-24 10:00:07 +0200

    LibGeneric: Add MustBeSameType concept
    Does exactly what the name suggests and allows you to check if two types
    are the same underlying type.

--- Library/LibGeneric/Traits.hpp
+++ Library/LibGeneric/Traits.hpp
@@ -11,4 +11,17 @@ namespace gen {
 
 	template<bool B, class T = void>
 	using enable_if_t = typename gen::__enable_if<B, T>::type;
+
+	template<typename A, typename B>
+	struct __IsSameType {
+		static constexpr bool value = false;
+	};
+
+	template<typename T>
+	struct __IsSameType<T, T> {
+		static constexpr bool value = true;
+	};
+
+	template<typename A, typename B>
+	concept MustBeSameType = __IsSameType<A, B>::value;
 }

611e227fb6a6ad9ee90621677d77b6262c9e7593
Muzuwi <kedziora99@gmail.com>
2024-12-10 23:12:05 +0100

    Arch: riscv64: Add stub vmkernel entry
    Add kernel image header required for booting the kernel with boot0.
    Replaced previous ad-hoc startup script with something more proper that
    properly sets up the C++ environment for the vmkernel. Added empty
    platform_boot_entry that currently just busy-loops forever.

--- Kernel/Arch/riscv64/Boot.s
+++ Kernel/Arch/riscv64/Boot.s
@@ -1,27 +1,153 @@
+.section .kernel_header, "ax"
 
-.section .bootstrap, "ax"
+/* Image header for boot0 to be able to boot the kernel */
+.globl _boot0_image_header
+_boot0_image_header:
+    .quad 0xF00DD00F
+    .quad __KERNEL_VM_BASE
+    .quad __KERNEL_VM_SIZE
+    .quad _riscv64_virtual_trampoline
 
-.globl _entrypoint_riscv64
-_entrypoint_riscv64:
-    # hifive unleashed UART
-    li t1, 0x10010000
+.section .entrypoint, "ax"
 
-    # set txen=1
-    li t0, 0x1
-    sw t0, 8(t1)
+.globl _init
 
-    li t0, 'H'
-    sw t0, 0(t1)
-    li t0, 'e'
-    sw t0, 0(t1)
-    li t0, 'l'
-    sw t0, 0(t1)
-    li t0, 'l'
-    sw t0, 0(t1)
-    li t0, 'o'
-    sw t0, 0(t1)
-    li t0, '!'
-    sw t0, 0(t1)
-.loop:
-    wfi
-    j .loop
+/* Jump trampoline to kernel in virtual memory */
+.globl _riscv64_virtual_trampoline
+_riscv64_virtual_trampoline:
+    /* Zero-initialize BSS section */
+    la t0, __SECTION_BSS_START
+    la t1, __SECTION_BSS_END
+.bss_zero_loop:
+    bge t0, t1, .bss_zero_done
+    sw zero, 0(t0)
+    addi t0, t0, 4
+    j .bss_zero_loop
+.bss_zero_done:
+    /* Configure a basic vector table */
+    la t0, early_trap_vector
+    csrw stvec, t0
+    /* Configure virtual kernel stack */
+    la t0, _virtual_startup_stack
+    addi sp, t0, 0
+    /* Preserve boot args from the previous bootstage */
+    addi sp, sp, -2*8
+    sd a0, 8(sp)
+    sd a1, 16(sp)
+
+    /* Call static initializers before running C++ code */
+    call _init
+
+    /* Restore boot args */
+    ld a1, 16(sp)
+    ld a0, 8(sp)
+    addi sp, sp, 2*8
+    /* Jump to the C++ entrypoint */
+    jal platform_boot_entry
+
+.dead:
+    j .dead
+
+.section .entrypoint.vector_table, "ax"
+
+.align 4
+
+.macro PUSH_ALL_REGISTERS
+    addi sp, sp, -32*8
+    sd x1, 1*8(sp)
+    sd x2, 2*8(sp)
+    sd x3, 3*8(sp)
+    sd x4, 4*8(sp)
+    sd x5, 5*8(sp)
+    sd x6, 6*8(sp)
+    sd x7, 7*8(sp)
+    sd x8, 8*8(sp)
+    sd x9, 9*8(sp)
+    sd x10, 10*8(sp)
+    sd x11, 11*8(sp)
+    sd x12, 12*8(sp)
+    sd x13, 13*8(sp)
+    sd x14, 14*8(sp)
+    sd x15, 15*8(sp)
+    sd x16, 16*8(sp)
+    sd x17, 17*8(sp)
+    sd x18, 18*8(sp)
+    sd x19, 19*8(sp)
+    sd x20, 20*8(sp)
+    sd x21, 21*8(sp)
+    sd x22, 22*8(sp)
+    sd x23, 23*8(sp)
+    sd x24, 24*8(sp)
+    sd x25, 25*8(sp)
+    sd x26, 26*8(sp)
+    sd x27, 27*8(sp)
+    sd x28, 28*8(sp)
+    sd x29, 29*8(sp)
+    sd x30, 30*8(sp)
+    sd x31, 31*8(sp)
+.endm
+
+.macro POP_ALL_REGISTERS
+    ld x31, 31*8(sp)
+    ld x30, 30*8(sp)
+    ld x29, 29*8(sp)
+    ld x28, 28*8(sp)
+    ld x27, 27*8(sp)
+    ld x26, 26*8(sp)
+    ld x25, 25*8(sp)
+    ld x24, 24*8(sp)
+    ld x23, 23*8(sp)
+    ld x22, 22*8(sp)
+    ld x21, 21*8(sp)
+    ld x20, 20*8(sp)
+    ld x19, 19*8(sp)
+    ld x18, 18*8(sp)
+    ld x17, 17*8(sp)
+    ld x16, 16*8(sp)
+    ld x15, 15*8(sp)
+    ld x14, 14*8(sp)
+    ld x13, 13*8(sp)
+    ld x12, 12*8(sp)
+    ld x11, 11*8(sp)
+    ld x10, 10*8(sp)
+    ld x9, 9*8(sp)
+    ld x8, 8*8(sp)
+    ld x7, 7*8(sp)
+    ld x6, 6*8(sp)
+    ld x5, 5*8(sp)
+    ld x4, 4*8(sp)
+    ld x3, 3*8(sp)
+    ld x2, 2*8(sp)
+    ld x1, 1*8(sp)
+    addi sp, sp, 32*8
+.endm
+
+
+.globl early_trap_vector
+early_trap_vector:
+.cfi_startproc
+    PUSH_ALL_REGISTERS
+
+    csrr a0, scause
+    csrr a1, sepc
+    csrr a2, stval
+
+.globl platform_trap_handler
+    jal platform_trap_handler
+
+    POP_ALL_REGISTERS
+    sret
+
+.trap_dead:
+    j .trap_dead
+.cfi_endproc
+
+.section .bss, "aw"
+
+.align 8
+
+/* The virtual entrypoint must have its own stack as well */
+_virtual_startup_stack_bottom:
+    .skip 16384
+.globl _virtual_startup_stack
+_virtual_startup_stack:
--- Kernel/Arch/riscv64/Platform.cpp
+++ Kernel/Arch/riscv64/Platform.cpp
@@ -1,1 +1,19 @@
-//  File intentionally left empty
+#include <Arch/Platform.hpp>
+#include <Core/Assert/Assert.hpp>
+#include <Core/Log/Logger.hpp>
+#include <SystemTypes.hpp>
+
+CREATE_LOGGER("rv64::boot", core::log::LogLevel::Debug);
+
+extern "C" {
+	[[maybe_unused]] void platform_trap_handler(uint64 scause, uint64 sepc, uint64 stval) {
+		log.error("Caught trap!");
+		log.error("SCAUSE={x} SEPC={x} STVAL={x}", scause, sepc, stval);
+		core::panic("Encountered an unhandled trap!");
+	}
+
+	[[maybe_unused]] [[noreturn]] void platform_boot_entry(void* /*fdt*/) {
+		//  For testing, simply do nothing in the actual kernel for now
+		HANG();
+	}
+}

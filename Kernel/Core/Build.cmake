752f6fc6369411f1652867ffac337dab97c8f242
Muzuwi <kedziora99@gmail.com>
2024-12-17 23:38:54 +0100

    Build: Enable all Core subsystems on all archs
    Now that there's no x86-specific bits left, we can actually build
    everything on everything.

--- Kernel/Core/Build.cmake
+++ Kernel/Core/Build.cmake
@@ -1,10 +1,7 @@
-if(${MU_MACHINE} STREQUAL "x86_64")
-    add_kernel_sources(MP/)
-    add_kernel_sources(Start/)
-endif()
-
 add_kernel_sources(Assert/)
 add_kernel_sources(Error/)
 add_kernel_sources(IRQ/)
 add_kernel_sources(Log/)
 add_kernel_sources(Mem/)
+add_kernel_sources(MP/)
+add_kernel_sources(Start/)

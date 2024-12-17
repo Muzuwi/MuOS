cad7898e2e020ceedee0c8b546b9966a75304acb
Muzuwi <kedziora99@gmail.com>
2024-12-17 23:36:56 +0100

    LibGeneric: Update platform allocator to core::mem heap
    KHeap no longer exists.

--- Library/LibGeneric/Platform/MuOS/Allocator-KHeap.cpp
+++ Library/LibGeneric/Platform/MuOS/Allocator-KHeap.cpp
@@ -1,10 +1,11 @@
 #include <Core/Assert/Panic.hpp>
+#include <Core/Mem/Heap.hpp>
 #include <LibGeneric/Allocator.hpp>
 
-void* gen::__platform_alloc(size_t) {
-	core::panic("gen::__platform_alloc not implemented!");
+void* gen::__platform_alloc(size_t n) {
+	return core::mem::hmalloc(n);
 }
 
-void gen::__platform_free(void*, size_t) {
-	core::panic("gen::__platform_free not implemented!");
+void gen::__platform_free(void* ptr, size_t) {
+	return core::mem::hfree(ptr);
 }

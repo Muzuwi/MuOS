98879434c9471ec8aeb25f4dd4b1f655bd294e5a
Muzuwi <kedziora99@gmail.com>
2024-12-17 23:36:56 +0100

    LibGeneric: Update platform allocator to core::heap
    Switch LibGeneric to use core::heap instead of KHeap, in preparation for
    its removal.

--- Library/LibGeneric/Platform/MuOS/Allocator-KHeap.cpp
+++ Library/LibGeneric/Platform/MuOS/Allocator-KHeap.cpp
@@ -1,10 +1,12 @@
+#include <Core/Assert/Panic.hpp>
+#include <Core/Mem/Heap.hpp>
 #include <LibGeneric/Allocator.hpp>
 #include <Memory/KHeap.hpp>
 
 void* gen::__platform_alloc(size_t n) {
-	return KHeap::instance().slab_alloc(n);
+	return core::mem::hmalloc(n);
 }
 
-void gen::__platform_free(void* p, size_t n) {
-	KHeap::instance().slab_free(p, n);
+void gen::__platform_free(void* p, size_t) {
+	return core::mem::hfree(p);
 }

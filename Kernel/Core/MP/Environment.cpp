894f5e44514dfb141a7ec3f7536631db808b6542
Muzuwi <kedziora99@gmail.com>
2025-02-09 11:25:52 +0100

    Core: MP: Add constinit to subsystem statics

--- Kernel/Core/MP/Environment.cpp
+++ Kernel/Core/MP/Environment.cpp
@@ -4,9 +4,9 @@
 #include <stddef.h>
 
 static constexpr const size_t max_supported_nodes = 32;
-static core::mp::Environment s_environments[max_supported_nodes] {};
-static size_t s_next_env {};
-static gen::Spinlock s_lock {};
+static constinit core::mp::Environment s_environments[max_supported_nodes] {};
+static constinit size_t s_next_env {};
+static constinit gen::Spinlock s_lock {};
 
 core::mp::Environment* core::mp::create_environment() {
 	gen::LockGuard lock { s_lock };

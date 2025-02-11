978ea7592af94b16621194a88098d8db6d6b4019
Muzuwi <kedziora99@gmail.com>
2025-02-11 21:27:51 +0100

    LibGeneric: Change gen::bitcast to use __builtin_bit_cast
    This allows actually using bitcast in a constexpr context.

--- Library/LibGeneric/Utility.hpp
+++ Library/LibGeneric/Utility.hpp
@@ -4,8 +4,6 @@ namespace gen {
 	template<typename Target, typename Source>
 	constexpr auto bitcast(Source const& v) -> Target {
 		static_assert(sizeof(Target) == sizeof(Source), "Type arguments for bitcast must be the same size.");
-		auto target = Target();
-		__builtin_memcpy(&target, &v, sizeof(Target));
-		return target;
+		return __builtin_bit_cast(Target, v);
 	}
 }

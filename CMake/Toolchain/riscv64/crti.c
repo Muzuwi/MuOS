7f4fd1341b8b07081c4611267996877f6187528e
Muzuwi <kedziora99@gmail.com>
2025-02-15 22:37:50 +0100

    Toolchain: riscv64: crti: Rename initializer arrays to use double scores
    This seems to be the more common name for init arrays, and my script I
    used for some ARM targets breaks as the name is double-scored there.
    
    Change the names to use double scores.

--- CMake/Toolchain/riscv64/crti.c
+++ CMake/Toolchain/riscv64/crti.c
@@ -1,17 +1,17 @@
 typedef void (*func_ptr)(void);
 
-extern func_ptr _init_array_start[0], _init_array_end[0];
-extern func_ptr _fini_array_start[0], _fini_array_end[0];
+extern func_ptr __init_array_start[0], __init_array_end[0];
+extern func_ptr __fini_array_start[0], __fini_array_end[0];
 
 void _init(void) {
-	for(func_ptr* func = _init_array_start; func != _init_array_end; func++)
+	for(func_ptr* func = __init_array_start; func != __init_array_end; func++)
 		(*func)();
 }
 
 void _fini(void) {
-	for(func_ptr* func = _fini_array_start; func != _fini_array_end; func++)
+	for(func_ptr* func = __fini_array_start; func != __fini_array_end; func++)
 		(*func)();
 }
 
-func_ptr _init_array_start[0] __attribute__((used, section(".init_array"), aligned(sizeof(func_ptr)))) = {};
-func_ptr _fini_array_start[0] __attribute__((used, section(".fini_array"), aligned(sizeof(func_ptr)))) = {};
+func_ptr __init_array_start[0] __attribute__((used, section(".init_array"), aligned(sizeof(func_ptr)))) = {};
+func_ptr __fini_array_start[0] __attribute__((used, section(".fini_array"), aligned(sizeof(func_ptr)))) = {};
